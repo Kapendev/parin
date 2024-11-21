d54610d430f01d1d9205e3c15007ac38383f39c0
Kapendev <alexandroskapretsos@gmail.com>
2024-11-21 15:42:32 +0200

    Update version.

--- source/parin/rl/rlgl.d
+++ source/parin/rl/rlgl.d
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 // Email: alexandroskapretsos@gmail.com
 // Project: https://github.com/Kapendev/parin
-// Version: v0.0.25
+// Version: v0.0.26
 // ---
 
 /// The `rlgl` module provides access to the rlgl.h functions.
