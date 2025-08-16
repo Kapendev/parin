149f9f4bf375ab03367ca3a2d4ce153d268db4f7
Kapendev <alexandroskapretsos@gmail.com>
2025-08-16 19:15:41 +0300

    Oops build stuff again.

--- scripts/run.bat
+++ scripts/run.bat
@@ -1,6 +1,7 @@
 @echo off
 setlocal
 
+set exe_path=.\app.exe
 set app_path=.\app.d
 set parin_package_path=.\parin_package
 set joka_package_path=.\joka_package
@@ -10,6 +11,7 @@ set "extra_flags="
 
 if not "%~1"=="" set compiler=%~1
 %compiler% ^
+    -of=%exe_path% ^
     -L=-L%parin_package_path%\vendor\%version% ^
     -J=parin -i %extra_flags% ^
     -run %app_path%
