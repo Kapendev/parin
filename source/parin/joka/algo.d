dcbc451486d2ecde33834ba3a8fb61d012fdb404
Kapendev <alexandroskapretsos@gmail.com>
2025-12-07 17:31:26 +0200

    Removed toStr from ValueIndex and fixed Nrv type.

--- source/parin/joka/algo.d
+++ source/parin/joka/algo.d
@@ -15,29 +15,22 @@ import parin.joka.types;
 
 @safe nothrow @nogc:
 
+alias NumericRangeValue = int;
+alias Nrv = NumericRangeValue;
+static assert(Nrv.min < 0, "Type `NumericRangeValue` should be a signed type.");
+
 struct ValueIndex(V, I) {
     V value;
     I index;
     alias value this;
-
-    @safe nothrow @nogc:
-
-    IStr toStr() {
-        return "{}".fmt(value);
-    }
-
-    IStr toString() {
-        return toStr();
-    }
 }
 
-alias NumericRangeValue = int;
-
+// NOTE: Maybe this should be a generic type, but ehhh.
 struct NumericRange {
-    NumericRangeValue start;
-    NumericRangeValue stop;
-    NumericRangeValue step;
-    NumericRangeValue index;
+    Nrv start;
+    Nrv stop;
+    Nrv step;
+    Nrv index;
 
     @safe nothrow @nogc:
 
@@ -45,7 +38,7 @@ struct NumericRange {
         return step > 0 ? index >= stop : index <= stop;
     }
 
-    NumericRangeValue front() {
+    Nrv front() {
         return index;
     }
 
@@ -53,8 +46,8 @@ struct NumericRange {
         index += step;
     }
 
-    NumericRangeValue back() {
-        return stop - (index - start) - 1;
+    Nrv back() {
+        return cast(Nrv) (stop - (index - start) - 1);
     }
 
     void popBack() {
@@ -62,21 +55,26 @@ struct NumericRange {
     }
 }
 
-NumericRange range(NumericRangeValue start, NumericRangeValue stop, NumericRangeValue step = 1) {
+NumericRange range(Nrv start, Nrv stop, Nrv step = 1) {
     return NumericRange(start, stop, step, start);
 }
 
-NumericRange range(NumericRangeValue stop) {
+NumericRange range(Nrv stop) {
     return range(0, stop);
 }
 
+@trusted
 auto toRange(T)(const(T)[] slice) {
     static struct Range {
-        const(T)[] slice;
-        Sz index;
+        // NOTE: It's using a pointer because it keeps the sturct small. Was something like 24LU with a slice.
+        const(T)* slice;
+        Nrv sliceLength;
+        Nrv index;
+
+        @trusted nothrow @nogc:
 
         bool empty() {
-            return index >= slice.length;
+            return index >= sliceLength;
         }
 
         T front() {
@@ -88,15 +86,15 @@ auto toRange(T)(const(T)[] slice) {
         }
 
         T back() {
-            return slice[$ - index - 1];
+            return slice[sliceLength - index - 1];
         }
 
         void popBack() {
             index += 1;
         }
 
-        Sz length() {
-            return slice.length;
+        Nrv length() {
+            return cast(Nrv) sliceLength;
         }
 
         T opIndex(Sz i) {
@@ -104,20 +102,18 @@ auto toRange(T)(const(T)[] slice) {
         }
     }
 
-    return Range(slice);
+    return Range(slice.ptr, cast(Nrv) slice.length);
 }
 
-auto enumerate(R)(R range, NumericRangeValue start = 0) {
-    static assert(!(is(R : const(A)[N], A, Sz N)), "Static arrays are not supported.");
-
+auto enumerate(R)(R range, Nrv start = 0) if (rangeIsNotStaticArrayType!R) {
     static if (is(R : const(T)[], T)) {
         return enumerate(range.toRange());
     } else {
-        alias FrontBack = ValueIndex!(typeof(R.front()), NumericRangeValue);
+        alias FrontBack = ValueIndex!(typeof(R.front()), Nrv);
 
         static struct Range {
             R range;
-            NumericRangeValue index;
+            Nrv index;
 
             bool empty() {
                 return range.empty;
@@ -148,9 +144,7 @@ auto enumerate(R)(R range, NumericRangeValue start = 0) {
     }
 }
 
-auto map(R, F)(R range, F func) {
-    static assert(!(is(R : const(A)[N], A, Sz N)), "Static arrays are not supported.");
-
+auto map(R, F)(R range, F func) if (rangeIsNotStaticArrayType!R) {
     static if (is(R : const(T)[], T)) {
         return map(range.toRange(), func);
     } else {
@@ -185,9 +179,7 @@ auto map(R, F)(R range, F func) {
     }
 }
 
-auto filter(R, F)(R range, F func) {
-    static assert(!(is(R : const(A)[N], A, Sz N)), "Static arrays are not supported.");
-
+auto filter(R, F)(R range, F func) if (rangeIsNotStaticArrayType!R) {
     static if (is(R : const(T)[], T)) {
         return filter(range.toRange(), func);
     } else {
@@ -218,9 +210,7 @@ auto filter(R, F)(R range, F func) {
     }
 }
 
-auto filterBack(R, F)(R range, F func) {
-    static assert(!(is(R : const(A)[N], A, Sz N)), "Static arrays are not supported.");
-
+auto filterBack(R, F)(R range, F func) if (rangeIsNotStaticArrayType!R) {
     static if (is(R : const(T)[], T)) {
         return filterBack(range.toRange(), func);
     } else {
@@ -251,17 +241,13 @@ auto filterBack(R, F)(R range, F func) {
     }
 }
 
-auto reduce(R, F, T)(R range, F func, T initial) {
-    static assert(!(is(R : const(A)[N], A, Sz N)), "Static arrays are not supported.");
-
+auto reduce(R, F, T)(R range, F func, T initial) if (rangeIsNotStaticArrayType!R) {
     static if (is(R : const(T)[], T)) {
         return reduce(range.toRange(), func, initial);
     } else {
-        enum isValueIndexType = is(typeof(R.front()) : const(ValueIndex!(V, I)), V, I);
-
         auto result = initial;
         foreach (item; range) {
-            static if (isValueIndexType) {
+            static if (rangeHasValueIndexType!R) {
                 result.value = func(result, item);
             } else {
                 result = func(result, item);
@@ -271,11 +257,11 @@ auto reduce(R, F, T)(R range, F func, T initial) {
     }
 }
 
-auto reduce(R, F)(R range, F func) {
+auto reduce(R, F)(R range, F func) if (rangeIsNotStaticArrayType!R) {
     static if (is(R : const(T)[], T)) {
         return reduce(range.toRange(), func);
     } else {
-        if (range.empty) return range.front.init;
+        if (range.empty) return typeof(range.front()).init;
         auto initial = range.front;
         range.popFront();
         return reduce(range, func, initial);
@@ -283,67 +269,50 @@ auto reduce(R, F)(R range, F func) {
 }
 
 auto min(R)(R range) {
-    enum isValueIndexType = is(typeof(R.front()) : const(ValueIndex!(V, I)), V, I);
-
-    static if (is(R : const(S)[], S)) {
-        alias T = typeof(range[0]);
-    } else static if (isValueIndexType) {
-        alias T = typeof(R.front().value);
-    } else {
-        alias T = typeof(R.front());
-    }
-
+    alias T = rangeFrontType!R;
     return range.reduce((T x, T y) => x < y ? x : y);
 }
 
 auto max(R)(R range) {
-    enum isValueIndexType = is(typeof(R.front()) : const(ValueIndex!(V, I)), V, I);
-
-    static if (is(R : const(S)[], S)) {
-        alias T = typeof(range[0]);
-    } else static if (isValueIndexType) {
-        alias T = typeof(R.front().value);
-    } else {
-        alias T = typeof(R.front());
-    }
-
+    alias T = rangeFrontType!R;
     return range.reduce((T x, T y) => x > y ? x : y);
 }
 
 auto sum(R)(R range) {
-    enum isValueIndexType = is(typeof(R.front()) : const(ValueIndex!(V, I)), V, I);
-
-    static if (is(R : const(S)[], S)) {
-        alias T = typeof(range[0]);
-    } else static if (isValueIndexType) {
-        alias T = typeof(R.front().value);
-    } else {
-        alias T = typeof(R.front());
-    }
-
-    return range.reduce((T x, T y) => x + y);
+    alias T = rangeFrontType!R;
+    return range.reduce((T x, T y) => cast(T) (x + y));
 }
 
 auto product(R)(R range) {
-    enum isValueIndexType = is(typeof(R.front()) : const(ValueIndex!(V, I)), V, I);
+    alias T = rangeFrontType!R;
+    return range.reduce((T x, T y) => cast(T) (x * y));
+}
 
+template rangeIsNotStaticArrayType(R) {
+    enum rangeIsNotStaticArrayType = !(is(R : const(A)[N], A, Sz N));
+}
+
+template rangeHasValueIndexType(R) {
+    enum rangeHasValueIndexType = is(typeof(R.front()) : const(ValueIndex!(V, I)), V, I);
+}
+
+template rangeFrontType(R) {
     static if (is(R : const(S)[], S)) {
-        alias T = typeof(range[0]);
-    } else static if (isValueIndexType) {
-        alias T = typeof(R.front().value);
+        R temp;
+        alias rangeFrontType = typeof(temp[0]);
+    } else static if (is(typeof(R.front()) : const(ValueIndex!(V, I)), V, I)) {
+        alias rangeFrontType = typeof(R.front().value);
     } else {
-        alias T = typeof(R.front());
+        alias rangeFrontType = typeof(R.front());
     }
-
-    return range.reduce((T x, T y) => x * y);
 }
 
 @safe nothrow @nogc
 unittest {
-    auto temp = 0;
-    auto start = 0;
-    auto stop = 4;
-    auto step = 1;
+    Nrv temp = 0;
+    Nrv start = 0;
+    Nrv stop = 4;
+    Nrv step = 1;
     assert(range(temp, stop, step).sum == 6);
     assert(range(temp, stop, step).enumerate().sum == 6);
     foreach (i; range(temp, stop, step)) {
@@ -371,23 +340,22 @@ unittest {
     int[] slice = array[];
     assert(slice.sum == 6);
 
-    assert(range(3).map((int x) => x * 2).sum == 6);
-    assert(range(9).filter((int x) => x == 2 || x == 4).sum == 6);
-    assert(range(0, 4).reduce((int x, int y) => x + y) == 6);
-    assert(range(0, 4).reduce((int x, int y) => x * y) == 0);
-    assert(range(1, 4).reduce((int x, int y) => x * y) == 6);
+    assert(range(3).map((Nrv x) => x * 2).sum == 6);
+    assert(range(9).filter((Nrv x) => x == 2 || x == 4).sum == 6);
+    assert(range(0, 4).reduce((Nrv x, Nrv y) => cast(Nrv) (x + y)) == 6);
+    assert(range(0, 4).reduce((Nrv x, Nrv y) => cast(Nrv) (x * y)) == 0);
+    assert(range(1, 4).reduce((Nrv x, Nrv y) => cast(Nrv) (x * y)) == 6);
     assert(slice.reduce((int x, int y) => x + y) == 6);
     assert(range(2, 6).min == 2);
     assert(range(2, 6).max == 5);
     assert(slice.min == 2);
     assert(slice.max == 2);
 
-    // You like my coode?
     assert(
         range(1, 5)
-            .map((int x) => x * 2)
-            .filter((int x) => x > 4)
-            .reduce((int a, int b) => a + b)
+            .map((Nrv x) => cast(Nrv) (x * 2))
+            .filter((Nrv x) => x > 4)
+            .reduce((Nrv a, Nrv b) => cast(Nrv) (a + b))
         == 14
     );
 }
