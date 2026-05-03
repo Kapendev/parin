47d2572e12661ef3fb776fac4e8a7658c18bc4a0
Kapendev <alexandroskapretsos@gmail.com>
2026-05-03 09:00:48 +0300

    More tween example stuff.

--- examples/basics/_024_smooth_toggle.d
+++ examples/basics/_024_smooth_toggle.d
@@ -1,22 +1,18 @@
-/// This example shows how to smoothly toggle between two states.
+/// This example shows how to slide a rectangle in and out of the screen.
 
 import parin;
 
-auto color = cyan;
 auto state = SmoothToggle();
+enum height = 100;
+enum offset = 8;
 
 bool update(float dt) {
-    if ('q'.isPressed && !state.isActive) state.toggle();
-    auto value = smoothstep(-resolutionHeight, resolutionHeight, state.update(dt));
-    if (state.isAtEnd) {
-        state.toggleSnap();
-        color.r = cast(ubyte) (randi % 255);
-        color.g = cast(ubyte) (randi % 255);
-        color.b = cast(ubyte) (randi % 255);
-    };
+    if ('q'.isPressed) state.toggle();
+    auto y = smoothstep(-height, offset, state.update(dt * 2));
+    auto color = Color(6, 66, 66, cast(ubyte) (state.now * 255));
 
     drawText("Press Q", resolution * 0.5, DrawOptions(Vec2(6), Hook.center));
-    drawRect(Rect(0, value, resolution), color);
+    drawRect(Rect(16, y, resolution.x - 32, height), color);
     return false;
 }
 
